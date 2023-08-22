fd0bf84e5a27a730d41d65cb25b5e9e48101d616
LakshithaCS <e17338@eng.pdn.ac.lk>
2023-08-22 11:30:16 +0530

    resnet scripts

---
+++ python-scripts-resnet/ECGDataSet.py
@@ -0,0 +1,54 @@
+import torch
+from torch.utils.data import  Dataset # wraps an iterable around the dataset
+import pandas as pd
+import os
+
+class ECGDataSet(Dataset):
+    
+    def __init__(self, split='train'):
+
+        self.split = split
+
+        # data loading
+        current_directory = os.getcwd()
+        # print("current = "+current_directory)
+        self.parent_directory = os.path.dirname(current_directory)
+        # print("parent = "+self.parent_directory)
+        train_small_path = os.path.join(self.parent_directory, 'data', 'deepfake-ecg-small', str(self.split) + '.csv')
+        # train_small_path = os.path.join(current_directory, 'data', 'deepfake-ecg-small', str(self.split) + '.csv')
+        # print("current = "+train_small_path)
+        self.df = pd.read_csv(train_small_path)  # Skip the header row
+        
+        # Avg RR interval
+        # in milli seconds
+        RR = torch.tensor(self.df['avgrrinterval'].values, dtype=torch.float32)
+        # calculate HR
+        self.y = 60 * 1000/RR
+
+        # Size of the dataset
+        self.samples = self.df.shape[0]
+
+    def __getitem__(self, index):
+        
+        # file path
+        filename= self.df['patid'].values[index]
+        asc_path = os.path.join(self.parent_directory,  'data', 'deepfake-ecg-small', str(self.split), str(filename) + '.asc')
+        # asc_path = os.path.join(os.getcwd(),  'data', 'deepfake-ecg-small', str(self.split), str(filename) + '.asc')
+        
+        ecg_signals = pd.read_csv( asc_path, header=None, sep=" ") # read into dataframe
+        ecg_signals = torch.tensor(ecg_signals.values) # convert dataframe values to tensor
+        
+        ecg_signals = ecg_signals.float()
+        
+        # Transposing the ecg signals
+        ecg_signals = ecg_signals/6000 # normalization
+        ecg_signals = ecg_signals.t() 
+        
+        qt = self.y[index]
+        # Retrieve a sample from x and y based on the index
+        return ecg_signals, qt
+
+    def __len__(self):
+        # Return the total number of samples in the dataset
+        return self.samples
+    
---
+++ python-scripts-resnet/KanResInit.py
@@ -0,0 +1,23 @@
+import torch.nn.functional as F
+from torch import nn
+
+class KanResInit(nn.Module):
+    def __init__(self, in_channels, filterno_1, filterno_2, filtersize_1, filtersize_2, stride):
+        #print(in_channels) --> 8
+        super(KanResInit, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels, filterno_1, filtersize_1, stride=stride)
+        self.bn1 = nn.BatchNorm1d(filterno_1)
+        self.conv2 = nn.Conv1d(filterno_1, filterno_2, filtersize_2)
+        self.bn2 = nn.BatchNorm1d(filterno_2)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        return x
+
+
+
